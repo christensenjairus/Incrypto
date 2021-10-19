6a8c439ab1254e376cc6ed2f87b94ed53f7594aa
Jairus Christensen <christensenjairus@gmail.com>
2021-10-19 14:54:33 -0600

    fixed sqlite3 error

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const { Console } = require('console');
 const net = require('net');
 const { exit } = require('process');
-const sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database(':memory:', (err) => {
-    if (err) {
-    return console.error(err.message);
-    }
-    console.log('Connected to the in-memory SQlite database.');
-});
+// const sqlite3 = require('sqlite3').verbose();
+// let db = new sqlite3.Database(':memory:', (err) => {
+//     if (err) {
+//     return console.error(err.message);
+//     }
+//     console.log('Connected to the in-memory SQlite database.');
+// });
 
 let chats = null;
 
