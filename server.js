245b6ce58ab806d85cc78864a405fb003382b740
christensenjairus@gmail.com <christensenjairus@gmail.com>
2021-10-19 21:28:07 -0600

    client/server side communication and launch.json

--- server.js
+++ server.js
@@ -1,3 +1,38 @@
+var WebSocketServer = require('websocket').server;
+var WebSocketClient = require('websocket').client;
+var WebSocketFrame  = require('websocket').frame;
+var WebSocketRouter = require('websocket').router;
+var W3CWebSocket = require('websocket').w3cwebsocket;
+const http = require('http');
+
+const server = http.createServer(function(request, response) {
+	// process HTTP request. Since we're writing just WebSockets
+	// server we don't have to implement anything.
+});
+
+server.listen(4040, function() { });
+ // create the server
+wsServer = new WebSocketServer({
+	httpServer: server
+});
+ // WebSocket server
+
+wsServer.on('request', function(request) {
+	var connection = request.accept(null, request.origin);
+	// This is the most important callback for us, we'll handle
+	// all messages from users here.
+	connection.on('message', function(message) {
+		if (message.type === 'utf8') {
+			// process WebSocket message
+		}
+	});
+	connection.on('close', function(connection) {
+		// close user connection
+	});
+});
+
+// what I had before -------------------------------------
+
 const { Console } = require('console');
 const net = require('net');
 const { exit } = require('process');
