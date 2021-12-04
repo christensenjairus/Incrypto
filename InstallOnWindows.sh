0dda396df281f8a8ad967c1b3080ba75500b145e
christensenjairus@gmail.com <christensenjairus@gmail.com>
2021-12-03 22:08:10 -0700

    small change

--- InstallOnWindows.sh
+++ InstallOnWindows.sh
@@ -3,7 +3,7 @@
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 cd $SCRIPTPATH
 cd ..
-mkdir .incryptoApp
+mkdir ~/.incryptoApp
 cp -r Incrypto ~/.incryptoApp/
 cd ~/.incryptoApp/Incrypto/
 npm i
