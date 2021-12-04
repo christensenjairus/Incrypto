747fdf6529c911e0160bc95dcbeec0d79e4653bc
christensenjairus@gmail.com <christensenjairus@gmail.com>
2021-12-03 22:40:19 -0700

    fix?

--- main.js
+++ main.js
@@ -32,13 +32,13 @@ const linuxAppAdd = createDesktopShortcut({
     }
 });
 const desktopShortcutsCreated = createDesktopShortcut({
-    windows: { filePath: 'C:\\Windows\\System32\\cmd.exe',
+    windows: { /*filePath: 'C:\\Windows\\System32\\cmd.exe'*/filePath: basepath + '/JustRun.bat',
                 name: 'Incrypto',
                 comment: 'Encrypted Messaging App',
                 icon: basepath + '/icons/hacker-25899.ico',
                 workingDirectory: basepath,
                 windowMode: "normal",
-                arguments: ' /k "npm start"' },
+                arguments: '' },
     linux:   { filePath: basepath + '/JustRun.sh',
                 name: 'Incrypto',
                 type: 'Application',
